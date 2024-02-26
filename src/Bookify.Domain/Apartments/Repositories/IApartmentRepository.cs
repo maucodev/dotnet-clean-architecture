1b81bfe4ae397d762cc16613a46e93eceedf5928
maucodev <hola@maucodev.com>
2024-02-26 13:34:30 -0600

    Adding the repositories

---
+++ src/Bookify.Domain/Apartments/Repositories/IApartmentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Bookify.Domain.Apartments.Repositories;
+
+public interface IApartmentRepository
+{
+    Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ src/Bookify.Domain/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Users.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    void Add(User user);
+}
