e6ab2573e1db2a8416f464f09471fafd902057d0
maucodev <hola@maucodev.com>
2024-02-26 17:52:22 -0600

    Creating the booking respository

---
+++ src/Bookify.Domain/Bookings/Repositories/IBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Apartments.Entities;
+using Bookify.Domain.Bookings.Entities;
+
+namespace Bookify.Domain.Bookings.Repositories;
+
+public interface IBookingRepository
+{
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken);
+
+    void Add(Booking booking);
+}
