27e2bec71adfa2131c2e223abeb92908020fe372
maucodev <hola@maucodev.com>
2024-02-26 11:35:57 -0600

    Add project files.

---
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,23 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity(Guid id)
+{
+    public Guid Id { get; init; } = id;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var other = (Entity)obj;
+
+        return Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
---
+++ src/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency(string Code)
+{
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+    public static readonly Currency Mxn = new("MXN");
+    
+    internal static readonly Currency None = new(string.Empty);
+
+    public string? Code { get; init; } = Code;
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+               throw new InvalidOperationException("The currency code is invalid");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All = [Usd, Eur, Mxn];
+}
