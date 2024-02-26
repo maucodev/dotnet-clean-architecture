62d43e20e4c3721cab7094074cfcb50370e6433f
maucodev <hola@maucodev.com>
2024-02-26 12:03:52 -0600

    Creating the users domain components

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,7 +2,24 @@
 
 public abstract class Entity(Guid id)
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+
     public Guid Id { get; init; } = id;
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents;
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 
     public override bool Equals(object? obj)
     {
--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -2,36 +2,27 @@
 
 namespace Bookify.Domain.Apartments;
 
-public sealed class Apartment : Entity
+public sealed class Apartment(
+    Guid id,
+    Name name,
+    Description description,
+    Address address,
+    Money price,
+    Money cleaningFee,
+    List<Amenity> amenities)
+    : Entity(id)
 {
-    public Apartment(
-        Guid id,
-        Name name,
-        Description description,
-        Address address,
-        Money price,
-        Money cleaningFee,
-        List<Amenity> amenities) : base(id)
-    {
-        Name = name;
-        Description = description;
-        Address = address;
-        Price = price;
-        CleaningFee = cleaningFee;
-        Amenities = amenities;
-    }
+    public Name? Name { get; private set; } = name;
 
-    public Name? Name { get; private set; }
+    public Description? Description { get; private set; } = description;
 
-    public Description? Description { get; private set; }
+    public Address? Address { get; private set; } = address;
 
-    public Address? Address { get; private set; }
+    public Money? Price { get; private set; } = price;
 
-    public Money? Price { get; private set; }
-
-    public Money? CleaningFee { get; private set; }
+    public Money? CleaningFee { get; private set; } = cleaningFee;
 
     public DateTime? LastBookedOnUtc { get; private set; }
 
-    public List<Amenity> Amenities { get; private set; }
+    public List<Amenity> Amenities { get; private set; } = amenities;
 }
