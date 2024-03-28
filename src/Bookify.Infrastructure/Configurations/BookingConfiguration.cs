751ad809009a63ac5eb9e559510659351c0e990e
maucodev <hola@maucodev.com>
2024-03-28 10:43:53 -0600

    Creating the Domain entity configurations

---
+++ src/Bookify.Infrastructure/Configurations/BookingConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Bookify.Domain.Apartments.Entities;
+using Bookify.Domain.Bookings.Entities;
+using Bookify.Domain.Shared;
+using Bookify.Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(booking => booking.Id);
+
+        builder.OwnsOne(booking => booking.PriceForPeriod, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.AmenitiesUpCharge, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.TotalPrice, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.Duration);
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(booking => booking.ApartmentId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(booking => booking.UserId);
+    }
+}
---
+++ src/Bookify.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Bookify.Domain.Apartments.Entities;
+using Bookify.Domain.Bookings.Entities;
+using Bookify.Domain.Reviews.Entities;
+using Bookify.Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("reviews");
+
+        builder.HasKey(review => review.Id);
+
+        builder.Property(review => review.Rating)
+            .HasConversion(rating => rating.Value, value => Rating.Create(value).Value);
+
+        builder.Property(review => review.Comment)
+            .HasMaxLength(200)
+            .HasConversion(comment => comment.Value, value => new Comment(value));
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(review => review.ApartmentId);
+
+        builder.HasOne<Booking>()
+            .WithMany()
+            .HasForeignKey(review => review.BookingId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(review => review.UserId);
+    }
+}
