da4e5bcbe9d462e38315bccefcc2fe9e55b544fb
maucodev <hola@maucodev.com>
2024-03-28 10:21:16 -0600

    Configuring infrastructure services with dependency injection

---
+++ src/Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Application.Abstractions.Email;
+
+namespace Bookify.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Domain.Users.Entities.Email email, Subject subject, Body body)
+    {
+        return Task.CompletedTask;
+    }
+}
