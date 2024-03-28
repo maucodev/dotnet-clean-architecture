1cd89b66d2ffe60de489d7208f96154e6cc09d38
maucodev <hola@maucodev.com>
2024-03-28 10:23:46 -0600

    Adding Entity Framework Core to the solution

---
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options), IUnitOfWork;
