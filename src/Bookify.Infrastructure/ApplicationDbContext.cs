515cd4b63933852316bf6559b66707f510adf98a
maucodev <hola@maucodev.com>
2024-03-28 11:37:17 -0600

    Automatically applying entity configurations

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -3,4 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
-public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options), IUnitOfWork;
+public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options), IUnitOfWork
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
