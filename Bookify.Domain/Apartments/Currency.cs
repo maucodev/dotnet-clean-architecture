6a6d5208528592745d46825c17170bd98ba0c43f
Mauricio Estrada <hola@maucodev.com>
2024-08-31 13:00:53 -0600

    Add the apartment domain

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Mxn = new("MXN");
+    
+    internal static readonly Currency None = new(string.Empty);
+
+    public static readonly IReadOnlyCollection<Currency> All = [Usd, Mxn];
+
+    public string Code { get; init; }
+
+    private Currency(string code)
+    {
+        Code = code;
+    }
+
+    public static Currency FromCode(string code)
+    {
+        return All.First(item => item.Code == code);
+    }
+}
