cc3e8e2803d3cc8c4c9e8bfdbd4deaaa0660bcd7
Mauricio Estrada <hola@maucodev.com>
2024-08-31 13:43:47 -0600

    Create repositories for domain entities

---
+++ Bookify.Domain/Apartments/Repository/IApartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bookify.Domain.Apartments.Entity;
+
+namespace Bookify.Domain.Apartments.Repository;
+
+public interface IApartmentRepository
+{
+    Task<Apartment> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
---
+++ Bookify.Domain/Users/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bookify.Domain.Users.Entity;
+
+namespace Bookify.Domain.Users.Repository;
+
+public interface IUserRepository
+{
+    Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    void Add(User user);
+}
