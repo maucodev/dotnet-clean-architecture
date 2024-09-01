027e7380f8690bb523daff616da355c887f0e1f6
Mauricio Estrada <hola@maucodev.com>
2024-08-31 19:58:07 -0600

    Add the clock and email services

---
+++ Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Bookify.Application.Abstractions.Email;
+
+namespace Bookify.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Domain.Users.Entity.Email recipient, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
