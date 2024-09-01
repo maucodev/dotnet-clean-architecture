9c60a1aa33411323ec58da84cb1c2273f6ce4f65
Mauricio Estrada <hola@maucodev.com>
2024-08-31 18:54:36 -0600

    Add the GetBooking query

---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string query = """
+             SELECT
+                id AS Id,
+                apartment_id AS ApartmentId,
+                user_id AS UserId,
+                status AS Status,
+                price_for_period_amount AS PriceAmount,
+                price_for_period_currency AS PriceCurrency,
+                cleaning_fee_amount AS PriceAmount,
+                cleaning_fee_currency AS PriceCurrency,
+                amenities_up_charge_amount AS PriceAmount,
+                amenities_up_charge_currency AS PriceCurrency,
+                total_price_amount AS PriceAmount,
+                total_price_currency AS PriceCurrency,
+                duration_start AS DurationStart,
+                duration_end AS DurationEnd,
+                created_on_utc AS CreatedOnUtc
+             FROM bookings
+             WHERE id = @BookingId
+             """;
+
+        var booking = await connection.QueryFirstOrDefaultAsync<BookingResponse>(
+            query,
+            new
+            {
+                request.BookingId
+            });
+
+        return booking;
+    }
+}
