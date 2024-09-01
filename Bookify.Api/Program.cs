eff914bc2ab9a75ecfa5594d646849a5efcff628
Mauricio Estrada <hola@maucodev.com>
2024-08-31 21:53:52 -0600

    Create the application layer

---
+++ Bookify.Api/Program.cs
@@ -0,0 +1,36 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
+namespace Bookify.Api;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddApplicationLayer();
+
+        builder.Services.AddInfrastructureLayer(builder.Configuration);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
