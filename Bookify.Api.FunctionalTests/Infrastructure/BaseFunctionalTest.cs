33603c81e62fb61d6cdbb8c26e6e858a36d5645a
Mauricio Estrada <hola@maucodev.com>
2024-09-13 21:10:03 -0600

    Add functional tests

---
+++ Bookify.Api.FunctionalTests/Infrastructure/BaseFunctionalTest.cs
@@ -0,0 +1,31 @@
+﻿using Bookify.Api.Modules.Users;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Bookify.Application.Users.LoginUser;
+using Bookify.Api.FunctionalTests.Mocks;
+
+namespace Bookify.Api.FunctionalTests.Infrastructure;
+
+public abstract class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
+{
+    protected readonly HttpClient HttpClient;
+
+    protected BaseFunctionalTest(FunctionalTestWebAppFactory factory)
+    {
+        HttpClient = factory.CreateClient();
+    }
+
+    protected async Task<string> GetAccessToken()
+    {
+        HttpResponseMessage loginResponse = await HttpClient.PostAsJsonAsync(
+            "api/v1/users/login",
+            new LogInUserRequest(
+                MockUserData.RegisterTestUserRequest.Email,
+                MockUserData.RegisterTestUserRequest.Password));
+
+        var accessTokenResponse = await loginResponse.Content.ReadFromJsonAsync<AccessTokenResponse>();
+
+        return accessTokenResponse!.AccessToken;
+    }
+}
---
+++ Bookify.Api.FunctionalTests/Mocks/MockUserData.cs
@@ -0,0 +1,8 @@
+﻿using Bookify.Api.Modules.Users;
+
+namespace Bookify.Api.FunctionalTests.Mocks;
+
+internal static class MockUserData
+{
+    public static RegisterUserRequest RegisterTestUserRequest = new("test@test.com", "test", "test", "12345");
+}
