62d4429ada7c5dacf0f5524a10da7025a4026b3c
Mauricio Estrada <hola@maucodev.com>
2024-09-13 20:06:21 -0600

    Add application layer unit tests

---
+++ Bookify.Application.UnitTests/Mocks/MockApartmentData.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Domain.Apartments.Entity;
+using Bookify.Domain.Shared;
+using System;
+
+namespace Bookify.Application.UnitTests.Mocks;
+
+internal static class MockApartmentData
+{
+    public static Apartment Create() => new(
+        Guid.NewGuid(),
+        new Name("Test apartment"),
+        new Description("Test description"),
+        new Address("Country", "State", "ZipCode", "City", "Street"),
+        new Money(100.0m, Currency.Usd),
+        Money.Zero(),
+        []);
+}
---
+++ Bookify.Application.UnitTests/Mocks/MockUserData.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Users.Entity;
+
+namespace Bookify.Application.UnitTests.Mocks;
+
+internal static class MockUserData
+{
+    public static User Create() => User.Create(FirstName, LastName, Email).Value;
+
+    public static readonly FirstName FirstName = new("First");
+    public static readonly LastName LastName = new("Last");
+    public static readonly Email Email = new("test@test.com");
+}
